3a34bad4494559057d2130493f1d56f14b13dcfe
Horkavagyok <gerahorka13@gmail.com>
2026-02-03 10:37:28 +0100

    Adószám és szabadnapok száma hozzáadása

--- WMSProject/EmployeeViewModel.cs
+++ WMSProject/EmployeeViewModel.cs
@@ -1,7 +1,11 @@
-﻿namespace WMSProject
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace WMSProject
 {
     public class EmployeeViewModel
     {
         public string Name { get; } = "Nagy Anna";
+        public string TaxNumber { get; set; } = "01234567-7-19";
+        public int DaysOff { get; set; } = 44235644;
     }
 }
